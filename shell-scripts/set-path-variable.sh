f64133f07aec7a65fa27632865d84f05b83a7de1
Byron Quezada <byron.quezada@e-campus.uab.cat>
2018-03-12 11:43:12 +0100

    Added PATH configuration script

---
+++ shell-scripts/set-path-variable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Adds the local root user /bin directory to the PATH variable to make the
+# binaries stored in this directory immediately available in the terminal.
+export PATH=$PATH:/home/root/bin
