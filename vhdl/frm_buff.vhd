bf0ecc3785739e23c5db489808e98f2f730d4f2a
Byron Quezada <byron.quezada@e-campus.uab.cat>
2018-04-23 13:54:36 +0200

    Added framebuffer vhdl draft

--- vhdl/frm_buff.vhd
+++ vhdl/frm_buff.vhd
@@ -1,6 +1,10 @@
--- ****************
--- * Frame Buffer *
--- ****************
+-- ***************************************************************************
+-- * Frame Buffer
+-- * The read and write address size is determined by the output draw area
+-- * that in this case is 128x64.
+-- * 128 -> 7 bits, 64 -> 6 bits
+-- * address size = 7 + 6 = 13
+-- ***************************************************************************
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -9,23 +13,22 @@ use ieee.numeric_std.all;
 entity frm_buff is
 
   generic(
-          ----------------
-          -- Dimensions --
-          ----------------
-
-          X_DIM     : integer :=  64;  -- Desired x dimension
-          X_DIM_BITS: integer :=   6;
-          Y_DIM     : integer := 480;  -- Desired y dimension
-          Y_DIM_BITS: integer :=   9;
-          BPP       : integer :=   8  -- Bits per pixel
+    -------------------------------------
+    -- Dimensions of the output draw area
+    -------------------------------------
+    X_DIM     : integer := 128;  -- Desired x dimension
+    X_DIM_BITS: integer :=   7;
+    Y_DIM     : integer :=  64;  -- Desired y dimension
+    Y_DIM_BITS: integer :=   6;
+    BPP       : integer :=   8  -- Bits per pixel
   );
 
   port(
-       clk  , rst         : in  std_logic;
-       bitmap_on, data_en : in  std_logic;
-       din                : in  std_logic_vector(23 downto 0);
-       r_addr, w_addr     : in  std_logic_vector(13 downto 0);
-       dout            : out std_logic_vector(23 downto 0)
+    clk       , rst     : in  std_logic;
+    bitmap_on , data_en : in  std_logic;
+    red, green, blue    : in  std_logic_vector( 7 downto 0);
+    r_addr    , w_addr  : in  std_logic_vector(13 downto 0);
+    rgb                 : out std_logic_vector(23 downto 0)
   );
 
 end frm_buff;
@@ -33,17 +36,19 @@ end frm_buff;
 architecture arch of frm_buff is
 
   signal bitmap_on: std_logic;
+  -- The size of the px vector is dependent on the resolution
   signal px_x , px_y : std_logic_vector(9 downto 0);
-  signal red_in, green_in, blue_in, red_out, green_out, blue_out:
-    std_logic_vector(BPP-1 downto 0);
-  signal w_addr, r_addr: std_logic_vector(X_DIM_BITS + Y_DIM_BITS - 1 downto 0);
+  signal r_in , g_in , b_in : std_logic_vector(BPP-1 downto 0);
+  signal r_out, g_out, b_out: std_logic_vector(BPP-1 downto 0);
+  signal w_addr, r_addr:
+    std_logic_vector(X_DIM_BITS + Y_DIM_BITS - 1 downto 0);
 
 begin
 
   -- memory inputs
-  red_in   <= rgb(23 downto 16);
-  green_in <= rgb(15 downto  8);
-  blue_in  <= rgb( 7 downto  0);
+  r_in <= red;
+  g_in <= green;
+  b_in <= blue;
   r_addr <= std_logic_vector(
     px_y(Y_DIM_BITS-1 downto 0) & px_x(X_DIM_BITS-1 downto 0)
   );
@@ -54,12 +59,12 @@ begin
                 DATA_WIDTH => BPP)
 
     port map(
-             clk    => clk,
-             we     => data_en,
-             w_addr => w_addr,
-             r_addr => r_addr,
-             d      => din_r,
-             q      => dout_r
+      clk    => clk,
+      we     => data_en,
+      w_addr => w_addr,
+      r_addr => r_addr,
+      d      => din_r,
+      q      => dout_r
     );
 
   ram_g_unit: entity work.ram(arch)
